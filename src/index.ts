d5c47c4a9a4ca3b34ff4c66cf32c967eab36774e
Abdullah Ahmed <abdullahahmed2004@gmail.com>
2025-05-09 16:20:52 +0500

    Basic Websocket server

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function connection(socket) {
+  socket.send("Hello from the server");
+  console.log("user connected");
+
+  socket.on("message", (rawData) => {
+    const strData = rawData.toString();
+    console.log(strData);
+    if (strData === "ping") {
+      socket.send("pong");
+    }
+  });
+});
+
+console.log("WebSocket server has started");
